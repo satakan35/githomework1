eed9d5714e966098c4b0da42d7ece520e601fa6b
satakan35 <satakan35@gmail.com>
2021-08-13 12:30:00 -0400

    third commit

--- src/githomework/GitHomeWork.java
+++ src/githomework/GitHomeWork.java
@@ -5,28 +5,21 @@ public class GitHomeWork {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		for(int i = 1; i <= 100; i++)
-        {
-            System.out.println(i);
-            
-         
-            
-            
-        }
+		
+		int n = 100;
+		System.out.print("Odd Numbers from 1 to "+n+" are: ");
+		for (int i = 1; i <= n; i++) {
+		   if (i % 2 != 0) {
+			System.out.print(i + " ");
 		
 		
-		
-		
+		   }
 		
 	}
 
 
 
-		
-		
-		
-		
-		
+		}
 		
 	}
 
